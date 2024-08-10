219785ced84d185216b8fcebbce1520f3776926e
Muhidin3 <muhidinshemsu3@gmail.com>
2024-08-10 09:10:38 +0300

    done
    noting just x o

--- js.js
+++ js.js
@@ -1,50 +1,112 @@
-mycancas = document.getElementById('canvas')
-c = mycancas.getContext('2d')
-
-mycancas.width = window.innerWidth
-mycancas.height = window.innerHeight
-
-
-inwid = window.innerWidth
-inhei = window.innerHeight
+let list_of_ids = ['a','b','c','d','e','f','g','h','i']
+let arrayBtn = document.querySelectorAll('td')
+let X_or_O = 'O'
+let iX_or_O = 'X'
+let is_game_ended = false
 
 
+// document.querySelectorAll('li')[2].style.color = 'cyan'
 
-Amplitude = 100
-wavelength = 0.001
-add = 0
-function loop() {
-    requestAnimationFrame(loop)
-    c.moveTo(0,inhei/2)
+document.querySelectorAll('td').forEach((v,i,a) => {
+    // v.style.color = '#153047'
+    v.id = list_of_ids[i]
+    // v.innerHTML = v.id
+    // v.innerHTML = list_of_ids[i].toUpperCase()
+});
+document.querySelectorAll('td').forEach((v,i,a) => { changeElement(v,i)});
+// document.querySelectorAll('td').forEach((v,i,a) => { checkWinner(v,i,a)});
 
-    c.clearRect(0,0,10000,1000)
 
-    c.beginPath()
-    for( let i = 0 ; i< inwid ; i++){
-
-        y = inhei/2 + Math.sin(i*wavelength+add)*Amplitude
-
-        c.lineTo(i,y)
-        c.fillRect(i,y,10,1000)
-
-        c.fillStyle='#ffffff'
-        // wavelength+=0.000000001
+function changeElement(a,i) {
+    a.addEventListener('click',()=>{
+        if (is_game_ended==false) {
+            
+            if(a.innerHTML !='X' && a.innerHTML!= 'O' ){
+                if (X_or_O == 'O') {
+                    X_or_O = 'X'
+                    iX_or_O = 'O'
+                }
+                else{
+                    X_or_O = 'O'
+                    iX_or_O = 'X'
+                }
+                a.innerHTML = X_or_O
+                // a.innerHTML = i
+                
+            }
+        }
         
-             
-        // console.log(Amplitude)        
+        
+    })
+    document.querySelector('table').onclick = ()=>{
+        document.querySelector('.turn').innerHTML = `${iX_or_O}'s turn`
+        checkWinner(arrayBtn) 
     }
-    add+=0.03
-    c.stroke()
-    
 }
-loop()
-       
+function checkWinner(arrayBtn) {
+    let a = document.getElementById('a')
+    let b = document.getElementById('b')
+    let c = document.getElementById('c')
+    let d = document.getElementById('d')
+    let e = document.getElementById('e')
+    let f = document.getElementById('f')
+    let g = document.getElementById('g')
+    let h = document.getElementById('h')
+    let i = document.getElementById('i')
+    
+
+    function algorithm(a,b,c){
+        if(a.innerHTML=='X'&& b.innerHTML=='X'&& c.innerHTML=='X'){
+
+            a.style.backgroundColor = '#127369'
+            b.style.backgroundColor = '#127369'
+            c.style.backgroundColor = '#127369'
+            console.log(a.innerHTML,b.innerHTML,c.innerHTML);
+            is_game_ended = true
+            end()
+        }
+        else if (a.innerHTML=='O'&& b.innerHTML=='O'&& c.innerHTML=='O'){
+            a.style.backgroundColor = '#127369'
+            b.style.backgroundColor = '#127369'
+            c.style.backgroundColor = '#127369'
+            is_game_ended = true
+            end()
+            
+        }
+        else{
+            return
+        }
+        return [a,b,c]
+        
+    }
+    algorithm(a,b,c)
+    algorithm(d,e,f)
+    algorithm(g,h,i)
+    algorithm(a,d,g)
+    algorithm(b,e,h)
+    algorithm(c,f,i)
+    algorithm(a,e,i)
+    algorithm(c,e,g)
+}
+
+function end() {
+    if(is_game_ended==true){
+        document.querySelector('.turn').innerHTML = `${X_or_O} wins`
+        document.querySelectorAll('td').forEach((v,i,a)=>{
+           v.addEventListener('click',()=>{
+            console.log('fhgjhdklmcad;k');
+            
+           }) 
+        })
+    }
+}
 
 
-
-
-
-
-
-
-console.log(mycancas.height/2)
+// the restart button
+document.querySelector('.btn').addEventListener('click',()=>{
+    document.querySelectorAll('td').forEach((v,i,a)=>{
+        v.innerHTML = ''
+        v.style.backgroundColor = document.querySelector('.wholeContainer').style.backgroundColor
+        is_game_ended = false
+    })
+})
